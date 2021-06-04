8cfa7500d7d9bc53764efed5ac3b35c1a4f15504
Sonu Thakur <sonu.concious@gmail.com>
2021-06-04 11:56:29 +0530

    added project structure

---
+++ source/PromotionEngine/PromotionEngine.Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PromotionEngine.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ source/PromotionEngine/PromotionEngine.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PromotionEngine.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
